c746ae1367a8feaca40203a7dc61b9756abf3976
loizzm <ireliontt@gmail.com>
2023-08-19 23:51:25 -0300

    adição docker

--- django/inmotion/inmotion/settings.py
+++ django/inmotion/inmotion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ir&)^xs1ixa2irz(a(o4w^4o*m+a#h0barc7$f4d6xtzfk)xj7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
